56f4f4a04ba9b94658a584b48a267727854b8d75
antoniorm80 <antoniorm80@hotmail.com>
2021-03-08 14:50:49 -0600

    Agregando el Login y Account
    Agregando el Login y Account mostrando un Overlay

---
+++ screens/account/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Login() {
+    return (
+        <View>
+            <Text> Modesto Martínez es el AMOR de mi VIDA</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
